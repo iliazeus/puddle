55526876ef25a23b550846b24b6f849b28d4e366
Ilia Pozdnyakov <iliazeus@proton.me>
2025-04-30 22:05:31 +0500

    make it possible to delete creations
    It's always good to be able to start from scratch.

--- backend.js
+++ backend.js
@@ -1,8 +1,24 @@
 import { blob } from "https://esm.town/v/std/blob";
+
 import { Context, Hono } from "npm:hono";
+import { bearerAuth } from "npm:hono/bearer-auth";
+
 import { z } from "npm:zod";
 
-const app = new Hono();
+let app = new Hono();
+
+let auth = bearerAuth({
+  // adapted from https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+  async verifyToken(token: string, c: Context) {
+    const msgUint8 = new TextEncoder().encode(token); // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join(""); // convert bytes to hex string
+    return hashHex === "57aafd0cb532573de7afb519a1d83002b51cf6f689f43b9a6281ee88df9de9b2";
+  },
+});
 
 app.get("/creations", async function getCreations(c: Context) {
   let creations = await blob.getJSON("/puddle/creations.json");
@@ -27,7 +43,7 @@ app.post("/creations", async function addCreation(c: Context) {
     type: z.string().required(),
     text: z.string().optional(),
   }).strip().parse(await c.req.json());
-  
+
   let creations = await blob.getJSON("/puddle/creations.json");
   newCreation.id = "https://iliazeus-puddle.web.val.run/creations/" + creations.items.length;
 
@@ -35,6 +51,11 @@ app.post("/creations", async function addCreation(c: Context) {
   await blob.setJSON("/puddle/creations.json", creations);
 
   return c.json(newCreation);
+});
+
+app.delete("/creations", auth, async function deleteAllCreations(c: Context) {
+  await blob.setJSON("/puddle/creations.json", { items: [] });
+  return c.json({ ok: true });
 });
 
 export default app.fetch;
