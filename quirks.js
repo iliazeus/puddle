dab94ff68f17b3954884b66a2e944420784f1348
Ilia Pozdnyakov <iliazeus@proton.me>
2025-05-01 19:49:07 +0500

    make it decentralized
    Since pondiverse.com implements a somewhat different API, I had to introduce a "quirks mode" library that performs the conversion. I might change pondiverse.com to match this new API, or I might change the puddle instead - need input from others to decide on that.

---
+++ quirks.js
@@ -0,0 +1,32 @@
+import * as lib from "./library.js";
+
+export async function fetchJson(url, init) {
+  url = new URL(url, window.location);
+  let json = await lib.fetchJson(url, init);
+
+  if (
+    url.origin ===
+      "https://todepond--33148208245911f0bc54569c3dd06744.web.val.run" &&
+    url.pathname.startsWith("/creations")
+  ) {
+    json.items = json.rows;
+    delete json.rows;
+
+    for (let item of json.items) {
+      item.uri =
+        "https://todepond--33148208245911f0bc54569c3dd06744.web.val.run/creations?json&c=" +
+        item.id;
+    }
+
+    if (json.items.length > 0) {
+      let nextUrl = new URL(url);
+      nextUrl.searchParams.set("page", (url.searchParams.get("page") ?? 1) + 1);
+    }
+  }
+
+  return json;
+}
+
+export async function postJson(url, body, init) {
+  return await lib.postJson(url, body, init);
+}
