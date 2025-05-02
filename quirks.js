25e1d2ed30cc1f311faa92c7015ffe1df92ba554
Ilia Pozdnyakov <iliazeus@proton.me>
2025-05-02 19:02:32 +0500

    adapt quirks mode to work with new pondiverse.com
    Some code died there. I might want to let some of this code die, too.

--- quirks.js
+++ quirks.js
@@ -1,17 +1,24 @@
 import * as lib from "./library.js";
 
-export async function fetchJson(url, init) {
-  url = new URL(url, window.location);
-  let json = await lib.fetchJson(url, init);
+async function fetchJsonFromPondiverseCom(url, init) {
+  url = new URL(url);
 
-  let origin = "https://todepond--33148208245911f0bc54569c3dd06744.web.val.run";
-  if (url.origin === origin && url.pathname.startsWith("/creations")) {
+  let baseUrl =
+    "https://todepond--33148208245911f0bc54569c3dd06744.web.val.run";
+
+  if (url.pathname === "/creations") {
+    url.pathname = "/get-creations";
+  }
+
+  if (url.pathname === "/get-creations") {
+    let json = await lib.fetchJson(url, init);
+
     json.items = json.rows;
     delete json.rows;
 
     for (let item of json.items) {
-      item.uri = `${origin}/creations?json&c=${item.id}`;
-      item.image = `${origin}/creations?c=${item.id}`;
+      item.uri = `${baseUrl}/get-creation?id=${item.id}`;
+      item.image = `${baseUrl}/get-creation-image?id=${item.id}`;
     }
 
     if (json.items.length > 0) {
@@ -22,9 +29,23 @@ export async function fetchJson(url, init) {
       );
       json.next = String(nextUrl);
     }
+
+    return json;
   }
 
-  return json;
+  return await lib.fetchJson(url, init);
+}
+
+export async function fetchJson(url, init) {
+  url = new URL(url, window.location);
+
+  if (
+    url.origin ===
+    "https://todepond--33148208245911f0bc54569c3dd06744.web.val.run"
+  )
+    return await fetchJsonFromPondiverseCom(url, init);
+
+  return await lib.fetchJson(url, init);
 }
 
 export async function postJson(url, body, init) {
