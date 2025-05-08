0051c78dff6d3cba3f1e5a06e0a2b74e94baf80a
Ilia Pozdnyakov <iliazeus@proton.me>
2025-05-08 17:57:45 +0500

    update stores list

--- stores.js
+++ stores.js
@@ -1,7 +1,7 @@
 export const stores = [
   {
     name: "todepondiverse",
-    home: "https://pondiverse.com/",
+    home: "https://todepond.com/pondiverse/",
     addCreation: "https://pondiverse.val.run/add-creation",
     getCreation: "https://pondiverse.val.run/get-creation?id=",
     getCreationImage: "https://pondiverse.val.run/get-creation-image?id=", // `creation.image` is null
@@ -14,5 +14,14 @@ export const stores = [
     getCreation: "https://api.iliazeus.lol/puddle/creations/",
     // getCreationImage: "TODO", // i have image urls inside creations currently
     getCreations: "https://api.iliazeus.lol/puddle/creations",
+  },
+  {
+    // elouan.xyz's store, currently just a fork of the todepondiverse store
+    name: "oceaniverse",
+    home: "https://oceaniverse.elouan.xyz",
+    addCreation: "https://theoceaniverse.val.run/add-creation",
+    getCreation: "https://theoceaniverse.val.run/get-creation?id=",
+    getCreationImage: "https://theoceaniverse.val.run/get-creation-image?id=",
+    getCreations: "https://theoceaniverse.val.run/get-creations",
   },
 ];
