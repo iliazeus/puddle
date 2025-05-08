979873671b772a7b546bbf19737e4bbfdb56d2f4
Ilia Pozdnyakov <iliazeus@proton.me>
2025-05-08 20:21:59 +0500

    add cart loader prototype

---
+++ tools.js
@@ -0,0 +1,98 @@
+export const tools = [
+  {
+    name: "screenpond",
+    types: ["screenpond"],
+    open: "https://screenpond.cool?creation=",
+    create: "https://screenpond.cool",
+  },
+  {
+    name: "chaoskit",
+    types: ["chaoskit"],
+    open: "https://evolved.systems/chaoskit/?creation=",
+    create: "https://evolved.systems/chaoskit/",
+  },
+  {
+    name: "pnmrsimjs",
+    types: [],
+    open: null,
+    create: "https://mags.omg.lol/simulator.php",
+  },
+  {
+    name: "CodePond",
+    types: ["CodePond"],
+    open: "https://codepond.elouan.xyz/?creation=",
+    create: "https://codepond.elouan.xyz/",
+  },
+  {
+    name: "cherry",
+    types: [],
+    open: null,
+    create: "https://cherry.cthulahoops.org/",
+  },
+  {
+    name: "collapse",
+    types: ["collapse"],
+    open: "https://collapse.cthulahoops.org/?creation=",
+    create: "https://collapse.cthulahoops.org/",
+  },
+  {
+    name: "bitart",
+    types: ["bitart"],
+    open: "https://iliazeus.lol/bitart/?creation=",
+    create: "https://iliazeus.lol/bitart/",
+  },
+  {
+    name: "real shaders",
+    types: ["real-shader"],
+    open: "https://garten.salat.dev/real-shaders.html?creation=",
+    create: "https://garten.salat.dev/real-shaders.html",
+  },
+  {
+    name: "fake shaders II",
+    types: ["fake-shader"],
+    open: "https://garten.salat.dev/fake-shaders2.html?creation=",
+    create: "https://garten.salat.dev/fake-shaders2.html",
+  },
+  {
+    name: "cellpond",
+    types: ["cellpond"],
+    open: "https://cellpond.cool?creation=",
+    create: "https://cellpond.cool",
+  },
+  {
+    name: "arrayer",
+    types: ["arrayer"],
+    open: "https://gaimeri.github.io/text-tools/arrayer.html?creation=",
+    create: "https://gaimeri.github.io/text-tools/arrayer.html",
+  },
+  {
+    name: "pondiverse",
+    types: ["*"],
+    open: "https://pondiverse.com/tool/?creation=",
+    create: "https://pondiverse.com/tool/",
+  },
+  {
+    name: "talise",
+    types: ["talise"],
+    open: "https://ernestum.net/talise/?id=",
+    create: "https://ernestum.net/talise/",
+  },
+  {
+    name: "meme",
+    types: ["*"],
+    open: "https://pondmemes.cthulahoops.org/?creation=",
+    create: null,
+  },
+  {
+    name: "gamify",
+    types: ["collapse", "styleshoot", "cellpond"],
+    open: "https://gamify.cthulahoops.org/?creation=",
+    create: null,
+  },
+  {
+    name: "styleshoot",
+    types: ["styleshoot"],
+    open: null,
+    create: "https://ponder.ooo/styleshoot.html",
+  },
+];
